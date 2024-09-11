0dda2bde47a21ea89a9b7839c9f36cb78f224553
IanBlan94 <143745602+IanBlan94@users.noreply.github.com>
2024-09-11 16:34:13 +0000

    Added another action webpage

--- M2T1/hello.py
+++ M2T1/hello.py
@@ -5,5 +5,23 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def index():
+    # multiquote for multiline
+    return """
+    <h1>Hello, World!</h1>
+    <p>This is a paragraph</p>
+    <ol>
+        <li>This</li>
+        <li>is</li>
+        <li>list</>
+    </ol>
+    <a href="action">Click me</a>
+    """
+
+@app.route("/action")
+
+def action():
+    return "Hello from the action route!"
+
+
+
