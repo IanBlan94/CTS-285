c22c843b4c3c57ed6525e908e072365cd7051a51
IanBlan94 <143745602+IanBlan94@users.noreply.github.com>
2024-09-17 14:16:29 -0400

    Update hello.py
    added more flask applications

--- M2T1/hello.py
+++ M2T1/hello.py
@@ -1,35 +1,25 @@
-#Minimal Flask app
-
+# minimal Flask app
 from flask import Flask, render_template, request, redirect, url_for
 
-
+#print("Starting flask program from ", __name__)
 app = Flask(__name__)
-
-app.config["DEBUG"] = True
-
+# Do any app specific setup here
+# for instance, loading a database
+app.config["DEBUG"] = True # will run in debug mode
+# TODO: make this a database or something
 comments = []
 
-@app.route("/",methods=["GET", "POST"])
-@app.route("/")
+@app.route("/", methods=["GET", "POST"]) # we allow posting
 def index():
-
     if request.method == "GET":
         return render_template("main_page.html", comments=comments)
+    # otherwise, it's a post
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
 
 
-    comments.apped(request.form["contacts"])
-    return redirector(url_for('index))
-
-
-
-    # multiquote for multiline
-   # name="Ian Blanchard"
-    #return render_template("main_page.html", name=name)
 
 @app.route("/action")
-
 def action():
     return "Hello from the action route!"
-
-
 
